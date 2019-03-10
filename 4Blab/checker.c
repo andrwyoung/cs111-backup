fc1663e47845bdcc9a9dacb48b06765e3c84d32e
andrwyoung <yong.andrew11@gmail.com>
2019-03-09 20:54:26 -0800

    yoookkooo oonnnoo
    pretty much done. turned in
    there is time skew, but it doesn't affect it since it's in the past

--- 4Blab/checker.c
+++ 4Blab/checker.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <mraa.h>
+
+#define SENSOR 0
 
 int main(int argc, char* argv[])
 {
-	fprintf(stderr, "hey\n");
+	mraa_init();
+
+	uint16_t sensor_value;
+	mraa_aio_context temp_sensor;
+
+	temp_sensor = mraa_aio_init(SENSOR);
+
+	sensor_value = mraa_aio_read(temp_sensor);
+	if(sensor_value == 0)
+	{
+		fprintf(stderr, "Sensor not working\n");
+		return 1;
+	}
 	return 0;
 }
 
